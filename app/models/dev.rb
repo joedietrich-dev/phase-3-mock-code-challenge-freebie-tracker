b3131319221181a4eb975a4097541d9155e2f56c
joedietrich-dev <joe@joedietrich.net>
2021-11-21 16:50:21 -0500

    commit tree 38c8891760402c880c3db20f1eb5d5294fee23b7

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    self.freebies.find_by(item_name: item_name) ? true : false
+  end
+
+  def give_away(dev, freebie)
+    if self.received_one?(freebie.item_name)
+      item = self.freebies.find_by(item_name: freebie.item_name)
+      item.dev = dev
+      item.save
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,32 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Coaster", value: 10, company: google, dev: rick)
+Freebie.create(item_name: "Coaster", value: 10, company: google, dev: morty)
+Freebie.create(item_name: "Coaster", value: 10, company: google, dev: meseeks)
+Freebie.create(item_name: "Coaster", value: 10, company: google, dev: gazorpazop)
+Freebie.create(item_name: "Mousepad", value: 82, company: facebook, dev: rick)
+Freebie.create(item_name: "Coke Can", value: 42, company: facebook, dev: morty)
+Freebie.create(item_name: "Chicken Parm", value: 69, company: facebook, dev: meseeks)
+Freebie.create(item_name: "Nametag", value: 102, company: dunder, dev: gazorpazop)
+Freebie.create(item_name: "Fountain Pen", value: 111, company: google, dev: rick)
+Freebie.create(item_name: "Cellphone", value: 72, company: dunder, dev: morty)
+Freebie.create(item_name: "Essential Oils", value: 20, company: enron, dev: meseeks)
+Freebie.create(item_name: "Desk Clock", value: 25, company: enron, dev: gazorpazop)
+Freebie.create(item_name: "Statue", value: 62, company: enron, dev: rick)
+Freebie.create(item_name: "Wine Bottle", value: 123, company: dunder, dev: morty)
+Freebie.create(item_name: "Mousepad", value: 14, company: enron, dev: meseeks)
+Freebie.create(item_name: "Wine Bottle", value: 123, company: dunder, dev: gazorpazop)
 
 puts "Seeding done!"
